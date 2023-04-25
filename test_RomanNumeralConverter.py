7dcc0aafef9cc9b84b41364cfa12e22a19626fb7
melody <melodylynnnh@gmail.com>
2023-04-25 13:30:48 -0400

    add caching test

--- test_RomanNumeralConverter.py
+++ test_RomanNumeralConverter.py
@@ -20,5 +20,15 @@ class TestRomanNumeralConverter(TestCase):
         with self.assertRaises(ValueError):
             self.converter.to_roman(4000)
 
+    def test_to_roman_cache(self):
+        # we can access private variables like this
+        # private variables aren't really "private" like they are in java
+        # they're what's called "mangled," which means we can access them
+        # with a more convoluted name, but we're not supposed to
+        # however, it is useful for testing purposes
+        self.converter._RomanNumeralConverter__arabic_to_roman_cache[1] = "test value"
+        self.assertEqual("test value", self.converter.to_roman(1))
+        # here, we assert that it doesn't convert the value anew, and rather
+        # sees that the value is in the cache, and just returns that
 
 if __name__ == '__main__': main()
